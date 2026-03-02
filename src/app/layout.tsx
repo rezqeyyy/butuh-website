373c78eef3dfd1148a786967196d6532e5cac78e
Rizqi Asan Masika <rizqiasanmm@gmail.com>
2026-03-02 09:29:02 +0700

    update: make clean code fot layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,14 @@
-// src/app/layout.tsx
 import "./globals.css";
-import NavbarWrapper from "@/components/navbar/NavbarWrapper"; // Memanggil otak pemilih navbar
+import NavbarWrapper from "@/components/navbar/NavbarWrapper";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="id">
-      <body className="bg-slate-50 pb-20 md:pb-0 md:pt-16">
+      {/* Tambahkan dark:bg-[#0a0a0a] dan transition-colors agar pergantian tema mulus */}
+      <body className="bg-gray-50 dark:bg-[#0a0a0a] text-gray-900 dark:text-white pb-20 md:pb-0 md:pt-16 transition-colors duration-300">
         
-        {/* Navbar dipanggil di sini. Dia akan mikir sendiri nampilin versi Merchant atau User */}
         <NavbarWrapper />
 
-        {/* Konten Utama */}
         <main className="min-h-screen">
           {children}
         </main>
